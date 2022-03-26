d34351476e2b35c20fe431ff261bc23efed6bb8b
Luis Felipe <86193136+luis-felipee@users.noreply.github.com>
2022-03-26 13:59:57 -0300

    Update ex57.py

--- ex57.py
+++ ex57.py
@@ -1,20 +1,3 @@
-# n = None
-# even = 0
-# odd = 0
-# while n != 0:
-#     try:
-#         n =  int(input("Digite um valor: "))
-#         if n % 2 == 0:
-#             even = even + 1
-#         if n % 2 != 0:
-#             odd = odd + 1
-#     except:
-#         print("Você não digitou um número!")
-#         continue
-# print('Acabou')
-# print(f"Even numbers: {even} ")
-# print(f"Odd numbers: {odd} ")
-
 g = None
 while True:
     try:
